1e58d742bf54d1700ab243fed50a26b054652c32
YeChan Eom <rune0509sp@gmail.com>
2020-11-22 16:41:02 +0900

    Added player movement-raycast

---
+++ Assets/Common/Scripts/MouseManager.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class MouseManager : MonoBehaviour
+{
+    public LayerMask clickableLayer; // layermask used to isolate raycasts against clickable layers
+
+    public Texture2D pointer; // normal mouse pointer
+    public Texture2D target; // target mouse pointer
+    public Texture2D doorway; // doorway mouse pointer
+
+    public EventVector3 onClickEnvironment;
+
+    void Update()
+    {
+        // Raycast into scene
+        RaycastHit hit;
+        if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 50, clickableLayer.value))
+        {
+            bool door = false;
+            if (hit.collider.gameObject.tag == "Doorway")
+            {
+                Cursor.SetCursor(doorway, new Vector2(16, 16), CursorMode.Auto);
+                door = true;
+            }
+            else
+            {
+                Cursor.SetCursor(target, new Vector2(16, 16), CursorMode.Auto);
+            }
+
+            if(Input.GetMouseButtonDown(0))
+            {
+                if(door)
+                {
+                    Transform doorway = hit.collider.gameObject.transform;
+                    onClickEnvironment.Invoke(doorway.position + doorway.forward * 10);
+                }
+                else
+                {
+                    onClickEnvironment.Invoke(hit.point);
+                }
+            }
+        }
+        else
+        {
+            Cursor.SetCursor(pointer, Vector2.zero, CursorMode.Auto);
+        }
+    }
+}
+
+[System.Serializable]
+public class EventVector3 : UnityEvent<Vector3> { }
+
